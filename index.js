cf0e0fe00ce6a42fe8eacbc22355a2c6e7994442
rafael moreira <rafa.siffert@gmail.com>
2023-08-02 22:26:31 -0400

    birthdays

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+ let Cards = (["Charlie", "Samip", "Ali"], "birthday"); 
+
+ function writeCards(names, event) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  const cards = writeCards(["Charlie", "Samip", "Ali"], "birthday");
+  console.log(cards);
+  
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+
+  countDown(10); // This will log the numbers from 10 to 0 in the console.
+
